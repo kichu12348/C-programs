0c96621676e01098bc8b8ca96b1f1a9b7d9e8cf5
Mahadevan Reji <rmahadevan574@gmail.com>
2024-11-12 17:43:39 +0530

    Refactor infix to postfix conversion algorithm for improved clarity and consistency

--- infixToPostfix.c
+++ infixToPostfix.c
@@ -83,60 +83,51 @@ Max=50
 stack[Max]
 top=-1
 
-
 push(item):
-    1.top=top+1
-    2.stack[top]=item
-
+    1. top = top + 1
+    2. stack[top] = item
 
 pop():
-    1.item=stack[top]
-    2.top=top-1
-    3.return item
+    1. item = stack[top]
+    2. top = top - 1
+    3. return item
 
 peek():
-    1.return stack[top]
+    1. return stack[top]
 
 isOperator(symbol):
-    1.return symbol == '^' || symbol == '*' || symbol == '/' || symbol == '+' || symbol == '-'
-
-isOperand(s):
-    1.return (s>= 'a' && s<= 'z') || (s>= 'A' && s<= 'Z')||(s>= '0' && s<= '9')
+    1. return symbol == '^' || symbol == '*' || symbol == '/' || symbol == '+' || symbol == '-'
 
 precedence(symbol):
-    1.if symbol == '^':
+    1. if symbol == '^':
         1.1 return 3
-
-    2.if symbol == '*' || symbol == '/':
+    2. if symbol == '*' || symbol == '/':
         2.1 return 2
-
-    3.if symbol == '+' || symbol == '-':
+    3. if symbol == '+' || symbol == '-':
         3.1 return 1
-        
-    4.return 0
+    4. return 0
 
 infixToPostfix(infix):
-    1.i=0, j=0
-    2.postfix[Max]
-    3.while infix[i]!='\0' :
-        3.1.token = infix[i]
-        3.2.if isOperand(token):
-            3.2.1 postfix[j++]=token
-        3.3.else if token=='(':
+    1. i = 0, j = 0
+    2. postfix[Max]
+    3. while infix[i] != '\0':
+        3.1 token = infix[i]
+        3.2 if isOperator(token):
+            3.2.1 while top != -1 && precedence(peek()) >= precedence(token):
+                3.2.1.1 postfix[j++] = pop()
+            3.2.2 push(token)
+        3.3 else if token == '(':
             3.3.1 push(token)
-        3.4.else if token==')':
-            3.4.1 while top!=-1 && peek()!='(':
-                3.4.1.1 postfix[j++]=pop()
+        3.4 else if token == ')':
+            3.4.1 while top != -1 && peek() != '(':
+                3.4.1.1 postfix[j++] = pop()
             3.4.2 pop()
-        3.5.else if isOperator(token):
-            3.5.1 while top!=-1 && precedence(peek())>=precedence(token):
-                3.5.1.1 postfix[j++]=pop()
-            3.5.2 push(token)
-        3.6.i++
-    4.while top!=-1:
-        4.1 postfix[j++]=pop()
-    5.postfix[j]='\0'
-    6.print postfix
-
+        3.5 else:
+            3.5.1 postfix[j++] = token
+        3.6 i++
+    4. while top != -1:
+        4.1 postfix[j++] = pop()
+    5. postfix[j] = '\0'
+    6. print postfix
 
 */
